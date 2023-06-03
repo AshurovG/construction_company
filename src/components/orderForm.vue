cd62d1136f2db6297de3c1e432e754df00d58544
Ashurov Georgy <ashurov1308@mail.ru>
2023-06-04 01:37:36 +0300

    create modal window for successful sending order form

--- src/components/orderForm.vue
+++ src/components/orderForm.vue
@@ -55,9 +55,28 @@
                     В тексте должно быть от 10 до 500 символов
                 </span>
             </div>
-            <button :disabled="(v$.fio.$invalid || v$.email.$invalid || v$.order.$invalid)" class="btn_send_email" type="submit" value="Send">Сделать заказ</button>
+            <button 
+            @click="showModal = true"
+            :disabled="(v$.fio.$invalid || v$.email.$invalid || v$.order.$invalid)"
+            class="btn_send_email" 
+            type="submit" 
+            value="Send">
+            Сделать заказ
+            </button>
+            
             <!-- <button class="btn_send_email" type="submit" value="Send">Сделать заказ</button> -->
         </form>
+        <div class="modal" v-show="showModal">
+            <div class="modal-content">
+                <img class="modal-image" src="../components/assets/pngwing.com.png" alt="">
+                <div class="modal-text">
+                    <p class="modal-title">Вы успешно офоромили заказ!</p>
+                    <p class="modal-subtitle">Ожидайте ответа на почту</p>
+                </div>
+            <button class="modal-btn" @click="showModal = false">Закрыть</button>
+            </div>
+            </div>
+        
     </div>
  </template>
 
@@ -107,11 +126,8 @@
         },
         data() {
             return {
-            flag: null
+                showModal: false
             }
-        },
-        mounted() {
-            this.flag = this.v$
         },
         methods: {
             // isValid() {
@@ -121,11 +137,11 @@
                 this.v$.$validate()
                 // console.log(this.flag)
                 if (!this.v$.$error){
-                    alert(('success'))
+                    // alert(('success'))
                 } else {
                     alert(('error'))
                 }
-                console.log(this.v$.fio.$invalid || this.v$.email.$invalid || this.v$.order.$invalid)
+                console.log(this.showModal)
                 
                 // try {
                 //     emailjs.sendForm('service_8dbscaj', 'template_9v0h7qn', e.target,
@@ -268,6 +284,82 @@
     color: red;
     margin-top: 5px;
     font-size: 16px;
+}
+
+.modal {
+  /* display: none; */
+  position: fixed;
+  z-index: 10000;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0,0,0,0.4);
+  border-radius: 40px;
+}
+
+.modal-title {
+    width: 100%;
+    height: 20%;
+    font-size: 18px;
+}
+
+.modal-content {
+    position: relative;
+    background-color: #fefefe;
+    margin: 15% auto;
+    padding: 20px;
+    border: 2px solid #888;
+    border-radius: 15px;
+    width: 80%;
+    height: 33%;
+}
+
+.modal-btn {
+    position: absolute;
+    right: 20px;
+    bottom: 20px;
+    width: 170px;
+    height: 44px;
+    padding: 8px 16px;
+    display: block;
+    text-align: center;
+    background: #4CBB17;
+    border-radius: 15px;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 22px;
+    line-height: 10px;
+    letter-spacing: -0.03em;
+    color: #F5F5F5;
+    cursor: pointer;
+    transition: background .2s linear;
+}
+
+.modal-btn:hover {
+    background: #32810e;
+}
+.modal-text {
+    text-align: center;
+    margin-top: 15px;
+}
+
+.modal-title {
+    width: 100%;
+    font-size: 25px;
+    font-weight: 700;
+}
+
+.modal-subtitle {
+    width: 100%;
+    font-size: 20px;
+    font-weight: 500;
+}
+
+.modal-image {
+    display: block;
+    margin: 0 auto;
 }
 
 </style>
